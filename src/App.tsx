140243c5ca1e4385c6236bda01ba2b3ab6ba4a0b
tusregalosdorados <tusregalosdorados@gmail.com>
2026-06-07 20:43:29 +0000

    fix: use HashRouter to avoid SPA 404 on direct navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import GastronomicoPage from "./pages/GastronomicoPage";
 import PeluqueriaPage from "./pages/PeluqueriaPage";
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/delantales-gastronomico" element={<GastronomicoPage />} />
@@ -34,7 +34,7 @@ const App = () => (
           <Route path="/admin/products/:id/edit" element={<ProductForm />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
